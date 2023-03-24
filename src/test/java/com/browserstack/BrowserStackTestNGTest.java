23757623a0ef4d758b334087138e5c98e82bf24d
Neha Agarwal <agarneha1331@gmail.com>
2023-03-24 17:43:57 +0530

    Made parallel test as default

--- src/test/java/com/browserstack/BrowserStackTestNGTest.java
+++ src/test/java/com/browserstack/BrowserStackTestNGTest.java
@@ -13,21 +13,52 @@ import org.json.simple.parser.JSONParser;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
 
 public class BrowserStackTestNGTest {
     public WebDriver driver;
     private Local l;
+    private static JSONObject config;
+    private static Map<String, Object> commonCapabilities;
+    private static String username;
+    private static String accessKey;
+
+    @BeforeSuite(alwaysRun=true)
+    @Parameters(value = { "config" })
+    public void beforeSuite(String config_file) throws Exception {
+        JSONParser parser = new JSONParser();
+        config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
+        commonCapabilities = (Map<String, Object>) config.get("capabilities");
+        
+        username = System.getenv("BROWSERSTACK_USERNAME");
+        if (username == null) {
+            username = (String) config.get("user");
+        }
+
+        accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+        if (accessKey == null) {
+            accessKey = (String) config.get("key");
+        }
+        try {
+          if ((commonCapabilities.get("browserstack.local") != null &&
+          commonCapabilities.get("browserstack.local").toString().equalsIgnoreCase("true") && (l == null))) {
+                  l = new Local();
+                  Map<String, String> options = new HashMap<String, String>();
+                  options.put("key", accessKey);
+                  l.start(options);
+              }
+        } catch (Exception e) {
+            System.out.println("Error while start local - " + e);
+        }
+    }
 
     @BeforeMethod(alwaysRun = true)
     @org.testng.annotations.Parameters(value = { "config", "environment" })
     @SuppressWarnings("unchecked")
     public void setUp(String config_file, String environment) throws Exception {
         JSONParser parser = new JSONParser();
-        JSONObject config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
+        config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
         JSONObject envs = (JSONObject) config.get("environments");
-
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("browserstack.source", "java-selenium:sample-selenium-3:v1.0");
 
@@ -47,33 +78,17 @@ public class BrowserStackTestNGTest {
             }
         }
 
-        String username = System.getenv("BROWSERSTACK_USERNAME");
-        if (username == null) {
-            username = (String) config.get("user");
-        }
-
-        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
-        if (accessKey == null) {
-            accessKey = (String) config.get("key");
-        }
-
-        if (capabilities.getCapability("browserstack.local") != null
-                && capabilities.getCapability("browserstack.local") == "true") {
-            l = new Local();
-            Map<String, String> options = new HashMap<String, String>();
-            options.put("key", accessKey);
-            l.start(options);
-        }
-
         driver = new RemoteWebDriver(
-                new URL("http://" + username + ":" + accessKey + "@" + config.get("server") + "/wd/hub"), capabilities);
+                new URL("https://" + username + ":" + accessKey + "@" + config.get("server") + "/wd/hub"), capabilities);
     }
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() throws Exception {
         driver.quit();
-        if (l != null) {
-            l.stop();
-        }
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void afterSuite() throws Exception {
+        if (l != null) l.stop();
     }
 }
